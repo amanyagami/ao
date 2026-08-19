fb3fa125199ddbe196314685d70ad9e6b4ca1b1d
Aleksandar Samardžić <asamardzic@openteams.com>
2026-08-19 10:58:37 +0000

    Add CuTeDSL kernel choice options for FP8 2:4 sparsity
    ghstack-source-id: 7e9053bfc213592d2eeffcb87df7e9c41cab3e39
    ghstack-comment-id: 5280066145
    Pull-Request: https://github.com/pytorch/ao/pull/4745

--- test/quantization/quantize_/workflows/float8/test_float8_sparse_2x4_2d_data_2d_metadata_tensor.py
+++ test/quantization/quantize_/workflows/float8/test_float8_sparse_2x4_2d_data_2d_metadata_tensor.py
@@ -117,6 +117,14 @@ class TestFloat8Sparse2x4_2DData2DMetadataTensor(common_utils.TestCase):
 
         self.assertEqual(tensor._sparse_kernel_choice, SparseKernelChoice.CUTLASS)
 
+    def test_config_sparse_kernel_choice_default(self):
+        config = Float8DynamicActivationFloat8WeightConfig(
+            version=2,
+            packing_format=Float8PackingFormat.SPARSE_2D_DATA_2D_METADATA,
+            granularity=PerRow(),
+        )
+        self.assertEqual(config._sparse_kernel_choice, SparseKernelChoice.CUTLASS)
+
     @unittest.skipIf(not is_sm_at_least_90(), "Need H100 to run")
     @unittest.skipIf(not _cutedsl_runtime_available(), "CuTeDSL runtime unavailable")
     @common_utils.parametrize(
@@ -221,7 +229,11 @@ class TestFloat8Sparse2x4_2DData2DMetadataTensor(common_utils.TestCase):
     @unittest.skipIf(not is_sm_at_least_90(), "Need H100 to run")
     @unittest.skipIf(not torch.cuda.is_available(), "Need CUDA available")
     @common_utils.parametrize("compile", [True, False])
-    def test_fp8_cutlass_sparse(self, compile):
+    @common_utils.parametrize("backend", list(SparseKernelChoice))
+    def test_fp8_cutlass_sparse(self, backend, compile):
+        if backend is SparseKernelChoice.CUTEDSL and not _cutedsl_runtime_available():
+            self.skipTest("CuTeDSL runtime unavailable")
+
         with torch.inference_mode():
             input = torch.rand((256, 256), dtype=torch.bfloat16, device="cuda")
             model = (
@@ -250,6 +262,7 @@ class TestFloat8Sparse2x4_2DData2DMetadataTensor(common_utils.TestCase):
                     version=2,
                     packing_format=Float8PackingFormat.SPARSE_2D_DATA_2D_METADATA,
                     granularity=PerRow(),
+                    _sparse_kernel_choice=backend,
                 ),
             )
             if compile:
